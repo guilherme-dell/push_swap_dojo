7c5eb85d852bbe48caa39661e1af1b8f8ba072c4
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-08-24 09:59:27 -0300

    Create push_swap.c

---
+++ sources/push_swap.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/22 18:27:41 by acesar-l          #+#    #+#             */
+/*   Updated: 2022/08/24 13:25:29 by acesar-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+int main(int argc, char **argv)
+{
+	t_data *data;
+
+	data = malloc(sizeof(t_data));
+	initialize(data, argc, argv);	
+}
