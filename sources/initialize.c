79849808a141d9ce728d7f28173679d97118ee01
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-08-24 11:52:31 -0300

    Primeiras alocações na stack_a

--- sources/initialize.c
+++ sources/initialize.c
@@ -35,16 +35,22 @@ void	check_arguments(t_data *data, int argc, char **argv)
 
 	i = 0;
 	arg = 1;
+	data->stack_a = malloc(sizeof(t_stack **));
+	
 	while (arg < argc)
 	{
-		while(argv[arg][i])
+		if(argv[arg][i])
 		{
-			if (ft_isdigit(argv[arg][i++]) == false)
+			while (argv[arg][i])
+			{
+				if (ft_isdigit(argv[arg][i++]) == false && argv[arg][0] != '-')
 				error(data, "Contains not integers as arguments");
-			stack_add_front(stack_a, stack_new(ft_atoi(argv[arg])));
-			data->stack_allocated = true;
+			}
+			stack_add_back(data->stack_a, stack_new(ft_atoi(argv[arg])));
+			print_stack_elements(*data->stack_a);
 		}
 		arg++;
+		i = 0;
 	}
 }
 
