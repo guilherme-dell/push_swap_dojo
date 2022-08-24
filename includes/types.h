0fc7d68519f258bab5fe8200cb8cbc1a3dc54795
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-08-24 11:53:56 -0300

    Suport Doubly Linked Lists

--- includes/types.h
+++ includes/types.h
@@ -6,7 +6,7 @@
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 18:28:38 by acesar-l          #+#    #+#             */
-/*   Updated: 2022/08/24 14:39:30 by acesar-l         ###   ########.fr       */
+/*   Updated: 2022/08/24 16:42:43 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ typedef struct s_stack
 
 typedef struct s_data
 {
-	t_bool		stack_allocated;
-	t_stack		**stack_a;
-	t_stack		**stack_b;
+	t_bool	stack_allocated;
+	t_stack **stack_a;
+	t_stack **stack_b;
 }	t_data;
