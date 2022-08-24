5810f03b1a5d8c41ed0399e6f2b31a6a5ad11abb
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-08-24 10:39:01 -0300

    Update push_swap.h

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -16,8 +16,14 @@
 # include "types.h"
 # include "defines.h"
 
-void	initialize(t_data *data, int argc, char **argv);
-void	init_data(t_data *data);
-void	error(t_data *data, char *message);
+void	  initialize(t_data *data, int argc, char **argv);
+void	  init_data(t_data *data);
+void	  error(t_data *data, char *message);
+void	  print_stack_elements(t_stack *stack);
+t_stack	*stack_new(int content);
+void	  stack_add_front(t_stack **stack, t_stack *new);
+t_stack	*stack_last_node(t_stack *stack);
+void	  stack_add_back(t_stack **stack, t_stack *new)
+void	  clear_stack(t_stack **stack);
 
 #endif
