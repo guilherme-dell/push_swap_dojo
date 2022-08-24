8895b6f763959e7edc66161a4a3524a3718a1c44
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-08-24 09:42:29 -0300

    Create types.h

---
+++ includes/types.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   types.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/22 18:28:38 by acesar-l          #+#    #+#             */
+/*   Updated: 2022/08/24 14:39:30 by acesar-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+typedef struct s_stack
+{
+	int		number;
+	struct s_stack	*next;
+	struct s_stack	*previous;
+}	t_stack;
+
+typedef struct s_data
+{
+	t_bool		stack_allocated;
+	t_stack		**stack_a;
+	t_stack		**stack_b;
+}	t_data;
