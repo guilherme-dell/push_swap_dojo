b4c0a0cf3be199f3edaeade1deeda13e1914f18c
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-08-27 21:59:45 -0300

    equals_numbers v1.1

--- sources/equals_numbers.c
+++ sources/equals_numbers.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   equals_numbers.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gnuncio- <gnuncio-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/28 00:06:03 by gnuncio-          #+#    #+#             */
-/*   Updated: 2022/08/28 00:32:06 by gnuncio-         ###   ########.fr       */
+/*   Updated: 2022/08/28 02:57:55 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	equals_numbers(char **values, int nbr_of_elements)
+void	equals_numbers(t_data *data, char **values, int nbr_of_elements)
 {
 	int	i;
 	int	j;
@@ -24,9 +24,8 @@ void	equals_numbers(char **values, int nbr_of_elements)
 		j = i;
 		while (j < nbr_of_elements)
 		{
-			if (ft_atoi(values[i]) == ft_atoi(values[j+1])){
-				ft_printf("FOUND NUMBER --> %s\n", values[i]);
-			}
+			if (ft_atoi(values[i]) == ft_atoi(values[j + 1]))
+				error(data, "Repeated numbers are not allowed.");
 			j++;
 		}
 		i++;
