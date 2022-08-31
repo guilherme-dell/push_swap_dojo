91a7bc3661745b08de31d4e2c1f60adb3a6d1163
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-08-31 19:16:09 -0300

    Update utils_2.c

--- sources/utils_2.c
+++ sources/utils_2.c
@@ -6,7 +6,7 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 09:32:47 by gnuncio-          #+#    #+#             */
-/*   Updated: 2022/08/31 19:47:36 by coder            ###   ########.fr       */
+/*   Updated: 2022/08/31 23:59:01 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,4 +38,19 @@ void swap_values(int *a, int *b)
 	*a ^= *b;
 	*b ^= *a;
 	*a ^= *b;
+}
+
+void	stack_remove_first_node(t_stack **stack)
+{
+	t_stack *new_first_node;
+
+	if ((*stack)->next == NULL)
+	{
+		free(*stack);
+		*stack = NULL;
+		return ;
+	}
+	new_first_node = (*stack)->next;
+	free(*stack);
+	*stack = new_first_node;
 }
