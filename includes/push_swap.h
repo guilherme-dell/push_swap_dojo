510c855bc2086371d2e41f1180615356a954f071
augustobecker <augustobecker.dev@gmail.com>
2022-09-02 02:16:37 +0200

    organize push_swap.c

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 18:28:43 by acesar-l          #+#    #+#             */
-/*   Updated: 2022/09/01 23:42:12 by coder            ###   ########.fr       */
+/*   Updated: 2022/09/02 01:48:02 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,19 @@
 # include "types.h"
 # include "defines.h"
 
+//sorting
+void		sort_three(t_data *data);
+
+//initalize
 void		initialize(t_data *data, int argc, char **argv);
 void		init_data(t_data *data, int argc);
+void		ft_isinteger(t_data *data, int argc, char **argv);
+int			find_last_leading_zero(char *number);
+t_bool		is_outside_integers_range(char *number);
+
+//memory_clean
+void		memory_clean(t_data *data);
 void		error(t_data *data, char *message);
-void		print_stack_elements(t_stack *stack);
 
 //stack_utils_1.c
 t_stack		*stack_new(int number);
@@ -32,9 +41,9 @@ void		clear_stack(t_stack **stack);
 void		stack_remove_first_node(t_stack **stack);
 int			stack_size(t_stack **stack);
 
+//normalization
 int			*normalization(char **values, int nbr_of_elements);
 void		fill_stacks(t_data *data, int *normalized_elements);
-void		print_vetor(int *vetor, int nbr_of_elements);
 void		equals_numbers(t_data *data, char **values, int nbr_of_elements);
 
 //operations_1.c
--- sources/push_swap.c
+++ sources/push_swap.c
@@ -6,13 +6,11 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 18:27:41 by acesar-l          #+#    #+#             */
-/*   Updated: 2022/09/01 23:40:45 by coder            ###   ########.fr       */
+/*   Updated: 2022/09/02 02:10:21 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-static void	printthe_stacks(t_data *data);
 
 int	main(int argc, char **argv)
 {
@@ -24,29 +22,5 @@ int	main(int argc, char **argv)
 	equals_numbers(data, argv, data->nbr_of_elements);
 	normalized_elements = normalization(argv, data->nbr_of_elements);
 	fill_stacks(data, normalized_elements);
-	printthe_stacks(data);
-	run_push(data, PB);
-	printthe_stacks(data);
-	run_swap(data, SS);
-	printthe_stacks(data);
-	run_reverse_rotate(data, RRA);
-	printthe_stacks(data);
-	run_push(data, PB);
-	printthe_stacks(data);
-	run_rotate(data, RA);
-	printthe_stacks(data);
-	run_push(data, PA);
-	printthe_stacks(data);
-	clear_stack(data->stack_a);
-	clear_stack(data->stack_b);
-	free(data);
-}
-
-static void	printthe_stacks(t_data *data)
-{
-	ft_printf("\nSTACK A");
-	print_stack_elements(*data->stack_a);
-	ft_printf("\nSTACK B");
-	print_stack_elements(*data->stack_b);
-	ft_printf("\n");
+	memory_clean(data);
 }
